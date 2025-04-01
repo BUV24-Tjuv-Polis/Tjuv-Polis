2c19134619197cf88530b308ef0177f5de1390e1
NoO1ee <dominik0119@hotmail.com>
2025-04-01 13:40:21 +0200

    Add project files.

---
+++ Tjuv-Polis/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Tjuv_Polis
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
